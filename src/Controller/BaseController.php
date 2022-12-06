ba8385bbae1111f9b4a70f60ed43ddfb0df0423f
malekronaldo064 <m.toukebri@cfa-insta.fr>
2022-12-06 12:32:57 +0100

    modification sur la page d'accueil

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class BaseController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(LivreRepository $livreRepository): Response
     {
+        $livres = $livreRepository->findAll();
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
+            'livres' => $livres,
         ]);
     }
 }
